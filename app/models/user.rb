0760bd17d85834286385f88f29559d033c5b6a14
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-16 18:04:58 +0900

    新規登録、ログイン、の入力規制実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
          validates :birth_date, presence: true
+  
+         validates :email,presence: true, uniqueness: { case_sensitive: false}
+         validates :password,presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
+
 
 end
