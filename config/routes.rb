c5071fd3c02b951421285db3bd1bc4af294cc1fa
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-16 16:24:58 +0900

    passwordカラムの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
   
   root to: "items#index"
   get 'items', to: 'items#index'
