532b2e357eb0d43cb991d2480d2152244d403e78
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-16 14:44:41 +0900

    deviseの導入とviewの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  #before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-    def basic_auth
-      authenticate_or_request_with_http_basic do |username, password|
-        username == 'tanimoto' && password == 'tanimoto1'
-      end
-    end
-
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'tanimoto' && password == 'tanimoto1'
+  end
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :firstname_kana, :birth_date])
+  end
+ 
 end
-
