e602f4f4e29b42acd6dbce469cf18573b52f19ba
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-17 11:26:19 +0900

    指摘事項の修正
    ・パスの記述方法の統一
    ・パスワード確認カラムの修正
    ・with_optionでバリデーションのまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :birth_date, presence: true
-  
-         validates :email,presence: true, uniqueness: { case_sensitive: false}
-         validates :password,presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
-         validates :last_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
+         with_options presence: true do
+
+         validates :nickname
+         validates :birth_date
+         validates :email, uniqueness: { case_sensitive: false}
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name_kana, format: {with:/\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana, format: {with:/\A[ァ-ヶー－]+\z/}
+         end
 
 
 end
