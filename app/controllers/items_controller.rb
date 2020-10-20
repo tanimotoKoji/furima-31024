af8d518111806535dd9f66f975c75f0f09d1ebbc
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-20 15:50:44 +0900

    set_itemメソッドの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit,:update]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-   @item = Item.find(params[:id])
   end
 
   def edit
-   @item = Item.find(params[:id])
    unless user_signed_in? && current_user.id == @item.user_id
     redirect_to action: :index
    end
@@ -31,7 +30,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.update(item_params)
       redirect_to action: :show
@@ -42,10 +40,13 @@ class ItemsController < ApplicationController
 
 
   private
+  
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :price,
        :postage_id, :prefecture_id, :handing_item_id, :image).merge(user_id:current_user.id)
+   end
 
+   def set_item
+    @item = Item.find(params[:id])
+   end
   end
-
-end
