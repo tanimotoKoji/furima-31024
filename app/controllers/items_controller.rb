28544184a148440c67524c0d2dabd950d82a8ace
tanimotoKoji <72075108+tanimotoKoji@users.noreply.github.com>
2020-10-20 17:11:34 +0900

    重複記述の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit,:update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_seller, only: [:edit, :destroy]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,12 +20,6 @@ class ItemsController < ApplicationController
   end
   end
 
-  def edit
-   unless user_signed_in? && current_user.id == @item.user_id
-    redirect_to action: :index
-   end
-   
-  end
 
   def update
     if @item.update(item_params)
@@ -49,4 +44,10 @@ class ItemsController < ApplicationController
    def set_item
     @item = Item.find(params[:id])
    end
+
+   def set_seller
+    unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path
+    end
   end
+end
